97daff47b29d135de878d925ebcebca6fcda804e
yassineamri722 <amriyassine722@gmail.com>
2025-08-16 16:02:42 -0400

    feat:testing locally with docker compose

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from flask_cors import CORS  # ← importer CORS
+
+app = Flask(__name__)
+CORS(app, origins=["http://backend:3000"])
+
+@app.route("/")
+def hello():
+    return jsonify(message="Hello from Flask backend!")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
